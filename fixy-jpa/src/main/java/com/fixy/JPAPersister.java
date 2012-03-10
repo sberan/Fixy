3718c7368b7620d657b4c1868aebc6267047fbad
Sam Beran <sberan@gmail.com>
2012-03-10 10:20:32 -0600

    make aggregator and add fixy-jpa

---
+++ fixy-jpa/src/main/java/com/fixy/JPAPersister.java
@@ -0,0 +1,26 @@
+package com.fixy;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+
+import com.pearson.fixy.Persister;
+
+public class JPAPersister implements Persister {
+
+    private final EntityManager entityManager;
+
+    public JPAPersister(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void persist(Object entity) {
+        if (entity.getClass().isAnnotationPresent(Entity.class)) {
+            entityManager.persist(entity);
+        }
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+}
