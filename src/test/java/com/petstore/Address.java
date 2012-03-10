df753b6f015f9b916966b2fd96d0834fcb6df42f
Sam Beran <sberan@gmail.com>
2012-03-10 09:31:55 -0600

    Revert "remove JPA tests"
    This reverts commit cd92783216cbab01c17c55e96024f9900e18448c.

---
+++ src/test/java/com/petstore/Address.java
@@ -0,0 +1,53 @@
+package com.petstore;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+    String city;
+    String country;
+    String stateOrProvince;
+    String postalCode;
+    String street;
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStateOrProvince() {
+        return this.stateOrProvince;
+    }
+
+    public void setStateOrProvince(String stateOrProvince) {
+        this.stateOrProvince = stateOrProvince;
+    }
+
+    public String getPostalCode() {
+        return this.postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getStreet() {
+        return this.street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+}
---
+++ src/test/java/com/petstore/Pet.java
@@ -0,0 +1,66 @@
+package com.petstore;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Pet {
+    @Id @GeneratedValue
+    Integer id;
+
+    @Column(nullable = false)
+    String name;
+    
+    @ManyToOne(optional = false)
+    PetType type;
+    
+    Long price;
+
+    @OneToOne(mappedBy = "pet")
+    Order order;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
